25f5db44941ade89dfe8ba44d406d5bfdf774772
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2019-06-01 12:09:08 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [name, setName] = useState('');
+  console.log('render');
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <h3>{name}</h3>
+      <input onChange={(event) => setName(event.target.value)} value={name} />
     </div>
   );
 };
 
-export default App;
+export default App
