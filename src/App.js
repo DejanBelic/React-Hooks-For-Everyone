258c5fe2c5054c860a19cbc6fe4a60e0c2e39501
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2019-06-01 12:53:15 +0200

    useState part 2

--- src/App.js
+++ src/App.js
@@ -9,9 +9,24 @@ const App = () => {
       <h1>Level Up Dishes</h1>
       <Toggle />
       <h3>{name}</h3>
-      <input onChange={(event) => setName(event.target.value)} value={name} />
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          formSubmit(name, setName);
+        }}>
+      <input
+        type="text"
+        onChange={(event) => setName(event.target.value)} value={name}
+      />
+      <button>Submit</button>
+      </form>
     </div>
   );
+};
+
+const formSubmit = (value, setValue) => {
+  console.log(`Form submitted! ${value}`);
+  setValue('');
 };
 
 export default App
