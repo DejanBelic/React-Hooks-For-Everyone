73d6911f171252f97d2bfa810d80ac8b122a9800
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2019-06-04 20:48:51 +0200

    What is cleanup.

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -3,6 +3,11 @@ import { useLayoutEffect } from 'react';
 export function useBodyScrollLock() {
   useLayoutEffect(() => {
     // Will wait to dom completes - so we will have all dom methods available.
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = 'hidden';
-  })
+
+   return () => {
+      document.body.style.overflow = originalOverflow;
+    }
+  },[])
 }
